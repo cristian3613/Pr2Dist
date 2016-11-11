7d1be6d8c5d571fd7e7a7ae2c640ae5d1778da95
Víctor <victorlafajasanz@MacBook-Pro.local>
2016-11-11 19:56:52 +0100

    borrando

--- src/Worker.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-
-public interface Worker extends Remote {
-	//Devuelve un vector con los primos entre min y max
-	ArrayList<Integer> encuentraPrimos(int min, int max)
-			throws RemoteException;
-}
--- src/ejecutar.sh
+++
@@ -1,3 +0,0 @@
-java -classpath bin -Djava.rmi.server.codebase=file:bin/ LanzarWorker &
-java -classpath bin -Djava.rmi.server.codebase=file:bin/ LanzarFactory &
-java -classpath bin Cliente 1 25 1
