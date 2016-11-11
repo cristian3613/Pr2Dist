1ecb00089bcc57fe268d4d36c24ab0c4e443f620
Víctor <victorlafajasanz@MacBook-Pro.local>
2016-11-11 20:03:14 +0100

    Borrando

--- src/WorkerFactory.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-
-public interface WorkerFactory extends Remote {
-	// Devuelve un vector de hasta n referencias a objetos Worker.
-	ArrayList<Worker> dameWorkers(int n) throws RemoteException;
-}
